758087f72b2749b5a28d431aead16c64452f856e
marionc22 <103053810+marionc22@users.noreply.github.com>
2022-11-16 09:48:05 +0100

    Add files via upload

---
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
@@ -0,0 +1,41 @@
+package com.wildcodeschool.wildandwizard.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "courses")
+    private List<Wizard> wizards = new ArrayList<>();
+
+    public Course() {
+    }
+    public List<Wizard> getWizards() {return wizards;}
+    public void setWizards(Wizard wizards) {this.wizards = (List<Wizard>) wizards;}
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
